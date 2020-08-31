6b0df450f59e9aaf59022eec9f8c663c60ffb937
Pankaj Singh <56686090+pankaj846@users.noreply.github.com>
2020-08-31 23:45:37 +0530

    hash map

---
+++ Hash Table/Subarray Sum Equals K.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int cummSum = 0, result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            cummSum += nums[i];
+            if (map.containsKey(cummSum - k)) {
+                result += map.get(cummSum - k);
+            }
+            map.put(cummSum, map.getOrDefault(cummSum, 0) + 1);
+        }
+    
+        return result;
+    }
+}
