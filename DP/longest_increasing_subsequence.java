2d26a906bf6ef69f9d826c77d85fc04cd87a6d4c
Pankaj Singh <56686090+pankaj846@users.noreply.github.com>
2020-11-01 01:39:04 +0530

    Create longest_increasing_subsequence.java

---
+++ DP/longest_increasing_subsequence.java
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/longest-increasing-subsequence/
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int memo[][] = new int[nums.length][nums.length];
+        return LIS(nums, -1, 0, memo);
+    }
+    
+    public int LIS(int arr[], int prev, int curr, int memo[][]){
+        if(curr==arr.length)
+            return 0;
+        
+        if(prev!=-1 && memo[prev][curr]>0)
+            return memo[prev][curr];
+        
+          int ans1=0;
+          if( prev==-1 || arr[prev]<arr[curr])
+             ans1 = 1 + LIS(arr, curr, curr+1, memo);
+           
+             int ans2 = LIS(arr, prev, curr+1, memo);
+            
+            if(prev!=-1)
+            memo[prev][curr]=Math.max(ans1,ans2);
+        
+            return Math.max(ans1, ans2);
+    }
+    
+    
+}
+
